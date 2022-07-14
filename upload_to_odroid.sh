07d8761c22fcdfa2fdbfdef680b560acc473784a
stefaan himpe <stefaan.himpe@gmail.com>
2022-07-14 17:05:05 +0200

    replace scp with rsync to deal with stalling networks

--- upload_to_odroid.sh
+++ upload_to_odroid.sh
@@ -3,7 +3,6 @@ if [ -z "$1" ]
 then
 	echo "Please pass ip address or hostname of odroid as first argument"
 else
-	scp publish.bz2 odroid@$1:/home/odroid/Projects/OdroidPianoteq
-	scp unpack_on_odroid.sh odroid@$1:/home/odroid/Projects/OdroidPianoteq
+	 rsync -avz --progress --partial publish.bz2 install_on_odroid.sh odroid@$1:~/Projects/OdroidPianoteq
 fi
 
