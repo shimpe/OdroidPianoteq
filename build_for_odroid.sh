5a3d877e9d3f0df6d6ed8992bcbce0830ea51bd8
stefaan himpe <stefaan.himpe@gmail.com>
2022-07-16 23:17:22 +0200

    make standalone build script compatible with vscode task so the install_on_odroid.sh script actually works in both cases

--- build_for_odroid.sh
+++ build_for_odroid.sh
@@ -2,5 +2,6 @@
 rm ./publish -rf
 rm ./pubish.bz2
 dotnet publish -r linux-arm64 -c Release --self-contained true -p:PublishTrimmed=true -p:PublishSingleFile=true -o ./publish
-tar cvjf publish.bz2 ./publish
+tar cvjf publish.bz2 -C ./publish .
+
 
